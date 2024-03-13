d7f0c124f7a2d269cd02fbf80bc7d04e7eb08d46
rSaydelleS <rafael.ppuerari@gmail.com>
2024-03-13 19:05:08 -0300

    finising project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'cyan': 'hsl(193, 38%, 86%)',
+        'neon': 'hsl(150, 100%, 66%)',
+        'darkb': 'hsl(218, 23%, 16%)',
+        'grayish': 'hsl(217, 19%, 24%)'
+      },
+      fontFamily: {
+        sans:['Mansrope', 'sans-serif']
+      }
     },
   },
   plugins: [],
